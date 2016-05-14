4fedbe061d0af9326d8e68d11297f2cdbc85a6cc
John Mille <john@ews-network.net>
2016-05-14 07:24:48 +0000

    Better cronjob script to avoid tens of threads Tags for all jobs

--- files/cronjob_deploy.sh
+++ files/cronjob_deploy.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
-for i in {1..4}; do
-    ansible-playbook /var/tmp/nat-to-dns/ansible-play/deploy-stack.yml -e ec2_url=`head -1 /var/tmp/ec2.conf` -e s3_url=`head -1 /var/tmp/s3.conf`
-    sleep 14
-done
+
+if ! [ -e /var/run/ansible-deploy.run ]; then
+    echo job is not running
+    touch /var/run/ansible-deploy.run
+
+    pass=1
+    for i in {1..4}; do
+	echo "Hello, I am going to run the ansible JLB Job - Pass $pass"
+	time ansible-playbook /var/tmp/nat-to-dns/ansible-play/deploy-stack.yml -e ec2_url=`head -1 /var/tmp/ec2.conf` -e s3_url=`head -1 /var/tmp/s3.conf` -e add_cron=false --skip-tags=packages
+	pass=$((pass+1))
+    done
+    rm -rfv /var/run/ansible-deploy.run
+else
+    echo "Job is already running - skipping"
+    exit 0
+fi
