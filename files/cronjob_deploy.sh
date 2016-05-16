ba157718a0d43f4a2857f57db2de9e00665cd2ff
John Mille <john@ews-network.net>
2016-05-16 13:21:33 -0400

    Fixed path to script

--- files/cronjob_deploy.sh
+++ files/cronjob_deploy.sh
@@ -7,7 +7,7 @@ if ! [ -e /var/run/ansible-deploy.run ]; then
     pass=1
     for i in {1..4}; do
 	echo "Hello, I am going to run the ansible JLB Job - Pass $pass"
-	time ansible-playbook /var/tmp/nat-to-dns/ansible-play/deploy-stack.yml -e ec2_url=`head -1 /var/tmp/ec2.conf` -e s3_url=`head -1 /var/tmp/s3.conf` -e add_cron=false --skip-tags=packages
+	time ansible-playbook /var/tmp/nat-to-dns/deploy-stack.yml -e ec2_url=`head -1 /var/tmp/ec2.conf` -e s3_url=`head -1 /var/tmp/s3.conf` -e add_cron=false --skip-tags=packages
 	pass=$((pass+1))
     done
     rm -rfv /var/run/ansible-deploy.run
